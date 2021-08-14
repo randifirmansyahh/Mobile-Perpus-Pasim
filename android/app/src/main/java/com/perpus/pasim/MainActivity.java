58082a1e74d2521d98f3b1f2714d79f2c9f6eae9
Randi Firmansyah <randykelvin26@gmail.com>
2021-08-15 06:02:15 +0700

    rename nama bundle jadi com.perpus.pasim

--- android/app/src/main/java/com/perpus/pasim/MainActivity.java
+++ android/app/src/main/java/com/perpus/pasim/MainActivity.java
@@ -1,4 +1,4 @@
-package com.junedomingo.perpus;
+package com.perpus.pasim;
 
 import com.facebook.react.ReactActivity;
 import android.os.Bundle;
--- android/app/src/main/java/com/perpus/pasim/MainApplication.java
+++ android/app/src/main/java/com/perpus/pasim/MainApplication.java
@@ -1,4 +1,4 @@
-package com.junedomingo.perpus;
+package com.perpus.pasim;
 
 import android.app.Application;
 import android.content.Context;
@@ -66,7 +66,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.junedomingo.perpus.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.perpus.pasim.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
