ca4e81dbf24854a812f2330687e664df9e26b6ee
Randi Firmansyah <randykelvin26@gmail.com>
2021-09-09 12:35:46 +0700

    normalize header

--- src/components/molecules/Header/index.js
+++ src/components/molecules/Header/index.js
@@ -1,38 +1,49 @@
 import React from 'react';
-import {Text, View, Image} from 'react-native';
+import {Text, View, Image, StyleSheet} from 'react-native';
+import normalize from 'react-native-normalize';
+
+//TODO header sudah normalize
 
 const Header = () => {
   return (
-    <View
-      style={{
-        backgroundColor: '#540000',
-        height: 70,
-        width: '100%',
-        flexDirection: 'row',
-        alignItems: 'center',
-        borderBottomRightRadius: 80,
-      }}>
-      <View
-        style={{
-          width: 50,
-          height: 50,
-          borderRadius: 50,
-          backgroundColor: 'white',
-          marginLeft: 16,
-        }}>
+    <View style={styles.box}>
+      <View style={styles.bgImage}>
         <Image
           source={require('../../../assets/images/logo/pasim.png')}
-          style={{
-            height: 50,
-            width: 50,
-            position: 'absolute',
-          }}></Image>
+          style={styles.image}
+        />
       </View>
-      <Text style={{color: 'white', fontSize: 16, marginLeft: 10}}>
-        Perpustakaan Universitas Nasional PASIM
-      </Text>
+      <Text style={styles.text}>Perpustakaan Universitas Nasional PASIM</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  box: {
+    backgroundColor: '#540000',
+    height: normalize(70),
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderBottomRightRadius: 80,
+  },
+  bgImage: {
+    width: normalize(50),
+    height: normalize(50),
+    borderRadius: normalize(50),
+    backgroundColor: 'white',
+    marginLeft: normalize(16),
+  },
+  text: {
+    fontSize: normalize(17),
+    color: 'white',
+    marginLeft: normalize(15),
+  },
+  image: {
+    height: normalize(50),
+    width: normalize(50),
+    position: 'absolute',
+  },
+});
 
 export default Header;
